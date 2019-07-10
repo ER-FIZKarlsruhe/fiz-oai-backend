b7995d45a2828fc2c66c3cf9532a1aa32ac76f64
sth <stefan.hofmann@fiz-karlsruhe.de>
2019-07-10 15:51:35 +0200

    Bugfix

--- src/main/java/de/fiz/oai/backend/service/impl/ItemServiceImpl.java
+++ src/main/java/de/fiz/oai/backend/service/impl/ItemServiceImpl.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.StringReader;
 import java.net.URL;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
