2029e79a9ba5ba2c61453af5867607dbbb9f903a
Luciani <all@fiz-karlsruhe.de>
2019-09-17 12:58:36 +0200

    XPathHelper.itMatches method

--- src/main/java/de/fiz/oai/backend/utils/XPathHelper.java
+++ src/main/java/de/fiz/oai/backend/utils/XPathHelper.java
@@ -1,8 +1,48 @@
 package de.fiz.oai.backend.utils;
 
+import java.io.IOException;
+import java.io.StringReader;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
 public class XPathHelper {
 
-  public static Boolean itMatches(final String content, final String xPath, final String value) {
+  public static Boolean itMatches(final String contentStr, final String xPathStr, final String valueStr) {
+
+    try {
+      DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+      factory.setNamespaceAware(true);
+      DocumentBuilder builder;
+      builder = factory.newDocumentBuilder();
+      Document doc = builder.parse(new InputSource(new StringReader(contentStr)));
+
+      XPathFactory xpathfactory = XPathFactory.newInstance();
+      XPath xpath = xpathfactory.newXPath();
+      
+      XPathExpression expr = xpath.compile(xPathStr);
+      Object result = expr.evaluate(doc, XPathConstants.NODESET);
+      NodeList nodes = (NodeList) result;
+      for (int i = 0; i < nodes.getLength(); i++) {
+        if (valueStr.equals(nodes.item(i).getNodeValue())) {
+          return true;
+        }
+      }
+      
+    } catch (ParserConfigurationException | SAXException | IOException | XPathExpressionException e) {
+      e.printStackTrace();
+    }
     return false;
   }
 }
