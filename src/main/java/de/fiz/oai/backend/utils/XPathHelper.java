c7f6c079d80c8d44c4178428710f771628be4bb7
Luciani <all@fiz-karlsruhe.de>
2019-09-17 13:01:07 +0200

    Check empty strings

--- src/main/java/de/fiz/oai/backend/utils/XPathHelper.java
+++ src/main/java/de/fiz/oai/backend/utils/XPathHelper.java
@@ -12,6 +12,7 @@ import javax.xml.xpath.XPathExpression;
 import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
 
+import org.apache.commons.lang3.StringUtils;
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
@@ -21,27 +22,29 @@ public class XPathHelper {
 
   public static Boolean itMatches(final String contentStr, final String xPathStr, final String valueStr) {
 
-    try {
-      DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-      factory.setNamespaceAware(true);
-      DocumentBuilder builder;
-      builder = factory.newDocumentBuilder();
-      Document doc = builder.parse(new InputSource(new StringReader(contentStr)));
+    if (!StringUtils.isBlank(contentStr) && !StringUtils.isBlank(xPathStr) && !StringUtils.isBlank(valueStr)) {
+      try {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        factory.setNamespaceAware(true);
+        DocumentBuilder builder;
+        builder = factory.newDocumentBuilder();
+        Document doc = builder.parse(new InputSource(new StringReader(contentStr)));
 
-      XPathFactory xpathfactory = XPathFactory.newInstance();
-      XPath xpath = xpathfactory.newXPath();
-      
-      XPathExpression expr = xpath.compile(xPathStr);
-      Object result = expr.evaluate(doc, XPathConstants.NODESET);
-      NodeList nodes = (NodeList) result;
-      for (int i = 0; i < nodes.getLength(); i++) {
-        if (valueStr.equals(nodes.item(i).getNodeValue())) {
-          return true;
+        XPathFactory xpathfactory = XPathFactory.newInstance();
+        XPath xpath = xpathfactory.newXPath();
+
+        XPathExpression expr = xpath.compile(xPathStr);
+        Object result = expr.evaluate(doc, XPathConstants.NODESET);
+        NodeList nodes = (NodeList) result;
+        for (int i = 0; i < nodes.getLength(); i++) {
+          if (valueStr.equals(nodes.item(i).getNodeValue())) {
+            return true;
+          }
         }
+
+      } catch (ParserConfigurationException | SAXException | IOException | XPathExpressionException e) {
+        e.printStackTrace();
       }
-      
-    } catch (ParserConfigurationException | SAXException | IOException | XPathExpressionException e) {
-      e.printStackTrace();
     }
     return false;
   }
