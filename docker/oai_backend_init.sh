86c84e573c717499db02828864e37255a5439f11
Michael Hoppe <michael.hoppe@fiz-karlsruhe.de>
2025-02-12 13:24:55 +0100

    integrate possibility to configure logfile-deletion

--- docker/oai_backend_init.sh
+++ docker/oai_backend_init.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 if [ -n "$PROXY" ]; then
-  ENV HTTPS_PROXY=proxy.fiz-karlsruhe.de:8888
-  ENV HTTP_PROXY=proxy.fiz-karlsruhe.de:8888
-  ENV https_proxy=proxy.fiz-karlsruhe.de:8888
-  ENV http_proxy=proxy.fiz-karlsruhe.de:8888
-  RUN echo "Acquire::http::Proxy \"http://proxy.fiz-karlsruhe.de:8888/\"; Acquire::https::Proxy \"http://proxy.fiz-karlsruhe.de:8888/\";" > /etc/apt/apt.conf && \
+  export HTTPS_PROXY=proxy.fiz-karlsruhe.de:8888
+  export HTTP_PROXY=proxy.fiz-karlsruhe.de:8888
+  export https_proxy=proxy.fiz-karlsruhe.de:8888
+  export http_proxy=proxy.fiz-karlsruhe.de:8888
+  echo "Acquire::http::Proxy \"http://proxy.fiz-karlsruhe.de:8888/\"; Acquire::https::Proxy \"http://proxy.fiz-karlsruhe.de:8888/\";" > /etc/apt/apt.conf && \
 fi
 
 apt-get update -y && apt-get upgrade -y
@@ -14,14 +14,15 @@ apt-get -y install sudo
 apt-get -y install vim
 apt-get clean
 rm -rf /var/lib/apt/lists/*
-chmod +x /usr/local/bin/oai_backend_init.sh
 
-set -e
+if [ -n "$LOGFILE_KEEP_DAYS" ]; then
+  set -e
+  echo -e "\
+  #0 1 * * * find /usr/local/some/logs -type f -ctime +$LOGFILE_KEEP_DAYS -delete\n\
+  0 2 * * * find /usr/local/tomcat/logs -type f -ctime +$LOGFILE_KEEP_DAYS -delete\
+  " | crontab -
 
-echo -e "\
-#0 1 * * * find /usr/local/some/logs -type f -ctime +200 -delete\n\
-0 2 * * * find /usr/local/tomcat/logs -type f -ctime +60 -delete\
-" | crontab -
+  crontab -l
+  cron
+fi
 
-crontab -l
-cron
