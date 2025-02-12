a642beaaf9f67de6e80b09464f14e98e4634afbd
Michael Hoppe <michael.hoppe@fiz-karlsruhe.de>
2025-02-12 12:54:38 +0100

    integrate possibility to integrate proxy

--- docker/oai_backend_init.sh
+++ docker/oai_backend_init.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+if [ -n "$PROXY" ]; then
+  ENV HTTPS_PROXY=proxy.fiz-karlsruhe.de:8888
+  ENV HTTP_PROXY=proxy.fiz-karlsruhe.de:8888
+  ENV https_proxy=proxy.fiz-karlsruhe.de:8888
+  ENV http_proxy=proxy.fiz-karlsruhe.de:8888
+  RUN echo "Acquire::http::Proxy \"http://proxy.fiz-karlsruhe.de:8888/\"; Acquire::https::Proxy \"http://proxy.fiz-karlsruhe.de:8888/\";" > /etc/apt/apt.conf && \
+fi
+
+apt-get update -y && apt-get upgrade -y
+apt-get -y install cron
+apt-get -y install sudo
+apt-get -y install vim
+apt-get clean
+rm -rf /var/lib/apt/lists/*
+chmod +x /usr/local/bin/oai_backend_init.sh
+
 set -e
 
 echo -e "\
