d4a6360bb73d1947cd887037abe165c7b9e58b0a
sth <stefan.hofmann@fiz-karlsruhe.de>
2025-06-02 12:18:38 +0200

    Bugfix

--- src/main/java/de/fiz/oai/backend/dao/impl/CassandraDAOItem.java
+++ src/main/java/de/fiz/oai/backend/dao/impl/CassandraDAOItem.java
@@ -203,8 +203,9 @@ public class CassandraDAOItem implements DAOItem {
 
     //TODO this is the slow synchronous approach. Replace it with the async one, see https://docs.datastax.com/en/developer/java-driver/4.4/manual/core/paging/#asynchronous-paging
     for (Row row : resultSet) {
-        if (i <= itemsToRetrieve) {
-            itemsRetrieved.add(populateItem(row));
+        itemsRetrieved.add(populateItem(row));
+        
+        if (i < itemsToRetrieve) {
             i++;
         } else {
             break;
