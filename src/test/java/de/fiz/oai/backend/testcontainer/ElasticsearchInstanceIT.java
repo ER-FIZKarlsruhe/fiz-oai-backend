83f14bb7920826e8c2ab5341bed13751e865116c
sth <stefan.hofmann@fiz-karlsruhe.de>
2025-06-04 09:26:46 +0200

    Extend "reindex" endpoint to be called with an item identifier

--- src/test/java/de/fiz/oai/backend/testcontainer/ElasticsearchInstanceIT.java
+++ src/test/java/de/fiz/oai/backend/testcontainer/ElasticsearchInstanceIT.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import de.fiz.oai.backend.models.Item;
 import de.fiz.oai.backend.models.SearchResult;
+import org.apache.http.HttpStatus;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 
@@ -18,7 +19,7 @@ public class ElasticsearchInstanceIT extends BaseInstance {
 
 
     @Test
-    public void testAllEndpoints() throws IOException {
+    public void testReindexAll() throws IOException {
         String template = new String(Files.readAllBytes(Paths.get("src/test/resources/radar-md-template.xml")));
 
         createFormat("oai_dc", "http://www.openarchives.org/OAI/2.0/oai_dc.xsd", "http://www.openarchives.org/OAI/2.0/oai_dc/");
@@ -75,8 +76,40 @@ public class ElasticsearchInstanceIT extends BaseInstance {
         }
 
         Assertions.assertEquals(11,countSearchWithMarks);
+    }
 
 
+    @Test
+    public void testReindexItem() throws IOException {
+        String template = new String(Files.readAllBytes(Paths.get("src/test/resources/radar-md-template.xml")));
+
+        createFormat("oai_dc", "http://www.openarchives.org/OAI/2.0/oai_dc.xsd", "http://www.openarchives.org/OAI/2.0/oai_dc/");
+        createFormat("radar", "https://radar-service.eu/schemas/descriptive/radar/v09/radar-dataset/", "http://radar-service.eu/schemas/descriptive/radar/v09/radar-dataset/");
+        createFormat("datacite", "https://schema.datacite.org/meta/kernel-4.0/metadata.xsd", "http://datacite.org/schema/kernel-4");
+
+        createCrosswalk("Radar2datacite", "radar", "datacite", "src/test/resources/RadarMD-v9.1-to-DataciteMD-v4_4.xslt");
+        createCrosswalk("Radar2OAI_DC_v09", "radar", "oai_dc", "src/test/resources/Radar2OAI_DC_v9.1.xsl");
+
+        createSet("testset", "testset", "this is a testset", List.of( "testtag"));
+
+        createItem("10.5072/38238", template);
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        checkItemsInIndex(1);
+        String result = searchItems("radar", true, null);
+        Assertions.assertTrue(result.contains("\"searchMark\":null"));
+        Assertions.assertTrue(result.contains("\"total\":1,\"size\":1"));
+
+        //reindex item okay
+        reindexItem("10.5072/38238", HttpStatus.SC_NO_CONTENT);
+
+        //reindex item not found
+        reindexItem("10.5072/3823fgdf8", HttpStatus.SC_NOT_FOUND);
     }
 
 
